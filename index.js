ecdf2ca44e88fadcd261d6a957ba704ce5494379
pabfou <pabfou@gmail.com>
2017-07-25 07:02:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-var pulse = require('adt-pulse');
-var myAlarm;
-var alarmstatus;
-var Service, Characteristic;
-var request = require("request");
+var tough = require('tough-cookie');
+var request = require('request');
+var q = require('q');
+var cheerio = require('cheerio');
+var adtstatus = 'unknown';
+var adtinstance = '';
+var adtunits = '';
 
-const rp = require('request-promise');
+//Cookie jar
+var j;
+
+//Request Configs
+var ua =  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
+
+var sat = '';
+var lastsynckey = '';
+var myAlarm = {
+	status:'',
+	newstate:'',
+	instance:'',
+	units:''
+};
+
+var Service, Characteristic;
 
 module.exports = function(homebridge){
 	Service = homebridge.hap.Service;
@@ -15,73 +32,214 @@ module.exports = function(homebridge){
 function adtpulseAccessory(log, config) {
 	this.log = log;
 	this.name = config["name"];
-
 	this.config = config;
-	this.httpMethod = config["http_method"] || "GET";
+	this.authenticated = false;
 	this.auth = {};
 	this.auth.username = config.username || "";
 	this.auth.password = config.password || "";
-	this.auth.immediately = config.immediately || "true";
 
-	myAlarm = new pulse(this.auth.username, this.auth.password);
-
-	// Register Callbacks:
-	myAlarm.onDeviceUpdate(
-			function(device) {
-					console.log(device)
-			}
-	);
-
-	//login and update alarm status
-	myAlarm.login();
-	myAlarm.updateAll();
+	this.urls = {
+		initialurl: 'https://portal.adtpulse.com/myhome/access/signin.jsp',
+		authUrl: 'https://portal.adtpulse.com/myhome/access/signin.jsp?e=n&e=n&&partner=adt',
+		sensorUrl: 'https://portal.adtpulse.com/myhome/ajax/homeViewDevAjax.jsp',
+		orbUrl: 'https://portal.adtpulse.com/myhome/ajax/orb.jsp',
+		statusChangeUrl: 'https://portal.adtpulse.com/myhome/quickcontrol/serv/ChangeVariableServ',
+		otherStatusUrl: 'https://portal.adtpulse.com/myhome/ajax/currentStates.jsp',
+		syncUrl: 'https://portal.adtpulse.com/myhome/Ajax/SyncCheckServ',
+		logoutUrl: 'https://portal.adtpulse.com/myhome/access/signout.jsp'
+	};
 
 }
 
 adtpulseAccessory.prototype = {
 
-//
-getState() {
-	//login and update alarm status
-	myAlarm.login();
-	myAlarm.updateAll();
-	alarmstatus = myAlarm.astatus();
+//login
+login: function(callback) {
+		console.log('Pulse: loggin in');
+		var that = this;
 
-	If (alarmstatus.contains('Disarmed'))
-		return Characteristic.SecuritySystemCurrentState.DISARMED;
+		j = request.jar();
 
-	If (alarmstatus.contains('Stay'))
-		return Characteristic.SecuritySystemCurrentState.STAY_ARM;
-
-	If (alarmstatus.contains('Away'))
-		return Characteristic.SecuritySystemCurrentState.AWAY_ARM;
-
+		request({
+			url: this.urls.authUrl,
+			followAllRedirects: true,
+			jar: j,
+			headers: {
+				'Host': 'portal.adtpulse.com',
+				'User-Agent': ua
+			},
+			form:{
+				username: that.config.username,
+				password: that.config.password
+			},
+			method: post
+		},
+		function(error, response, body) {
+			if(err || httpResponse.req.path !== '/myhome/6.18.0-238/summary/summary.jsp'){
+				that.authenticated = false;
+				console.log('Pulse: Authentication Failed');
+				callback(error);
+			} else {
+				that.authenticated = true;
+				console.log('Pulse: Authentication Success');
+				callback(error, response, body)
+			}
+		})
 	},
 
-setState(targetState) {
+	logout: function () {
+		console.log('Pulse: logout');
 
-	//login and update alarm status
-	myAlarm.login();
-	myAlarm.updateAll();
+		var that = this;
 
-	var changeState;
+		request(
+			{
+				url: this.urls.logoutUrl,
+				jar: j,
+				headers: {
+					'User-Agent': ua
+				}
+			},
+			function () {
+				that.authenticated = false;
+			}
+		)
+	},
 
-	changeState.instance = myAlarm.instance();
-	changeState.units = myAlarm.units();
-	changeState.newstate = targetState;
 
-	myAlarm.setAlarmState(changeState);
+//get alarm status
+getState: function(callback) {
+	console.log('Pulse: Getting Alarm Statuses');
+	that = this;
+	this.request({
+			url: that.urls.config.orbUrl,
+			jar: j,
+			headers: {
+				'User-Agent': ua
+			},
+		},
+		function(err, httpResponse, body) {
+
+			if (error) {
+				this.log('GetState function failed: %s', error.message);
+				callback(error);
+			} else {
+
+			var actions = [];
+
+			//get the sat code
+			sat = body.match(/sat=([^&]*)&/)[1];
+
+			//parse the html
+			$ = cheerio.load(body);
+
+			//grab the action buttons
+			$('#divOrbSecurityButtons input').each(function(el){
+
+			//get the args from the onclick
+			var matches = $(this).attr('onclick').match(/'([^']*)'/g);
+
+			//remove the singl quotes
+			matches.forEach(function(obj, index, arr){
+					arr[index] = obj.replace(/'/gi,'');
+				});
+
+			//populate the values
+			myAlarm = {
+					instance: matches[0],
+					newstate:matches[1],
+					units: matches[2]
+			  }
+			});
+
+			myAlarm = {
+				status: $('#divOrbTextSummary span').text()
+			};
+
+			if (myAlarm.status.contains('stay')) {
+				state = "0";
+			};
+
+			if (myAlarm.status.contains('away')) {
+				state = "1";
+			};
+
+			if (myAlarm.status.contains('night')) {
+				state = "2";
+			};
+
+			if (myAlarm.status.contains('disarmed')) {
+				state = "3";
+			};
+
+			if (myAlarm.status.contains('trigger')) {
+				state = "4";
+			};
+
+			this.log("State is currently %s", myAlarm.status);
+			callback(state);
+		}
+		}.bind(this));
 
 },
 
-getCurrentState: function() {
-	this.log("Getting current state");
-	this.getState();
+setTargetState: function(state, callback) {
+	this.log("Setting state to %s", state);
+	this.login(callback);
+
+	var targetstate;
+	var self = this;
+	switch (state) {
+		case Characteristic.SecuritySystemTargetState.STAY_ARM:
+			targetstate = 'Stay';
+			break;
+		case Characteristic.SecuritySystemTargetState.AWAY_ARM :
+			targetstate = 'Away';
+			break;
+		case Characteristic.SecuritySystemTargetState.NIGHT_ARM:
+		targetstate = 'Night';
+			break;
+		case Characteristic.SecuritySystemTargetState.DISARM:
+		targetstate = 'Disarmed';
+			break;
+	}
+
+	var url = this.url.config.statusChangeUrl+'?rtn=xml&fi='+encodeURIComponent(myAlarm.instance)+'&vn=arm-state&u='+encodeURIComponent(myAlarm.units)+'&ft=security-panel&sat=' + sat + '&value=' + encodeURIComponent(targetstate);
+
+	request(
+		{
+			url: url,
+			jar: j,
+			headers: {
+				'User-Agent': ua,
+				'Referer': 'https://portal.adtpulse.com/myhome/summary/summary.jsp'
+			},
+		},
+		function(err, httpResponse, body) {
+			if(err){
+				console.log('SetState function failed: %s', error.message);
+				callback(error);
+			} else {
+				console.log('Pulse setAlarmState Success');
+				self.securityService.setCharacteristic(Characteristic.SecuritySystemCurrentState, state);
+				callback(error, response, state);
+			}
+		}.bind(this));
+		callback(null);
 	},
-getTargetState: function() {
+
+getCurrentState: function(callback) {
+		this.log("Getting current state");
+		this.login(callback);
+		this.getState(callback);
+},
+
+getTargetState: function(callback) {
 	this.log("Getting target state");
-	this.getState();
+	this.login(callback);
+	this.getState(callback);
 },
+
 identify: function(callback) {
 	this.log("Identify requested!");
 	callback(); // success
@@ -92,12 +250,12 @@ getServices: function() {
 
 				this.securityService
 							.getCharacteristic(Characteristic.SecuritySystemCurrentState)
-							.on('get', this.getCurrentState());
+							.on('get', this.getCurrentState.bind(this));
 
 				this.securityService
 							.getCharacteristic(Characteristic.SecuritySystemTargetState)
-							.on('get', this.getTargetState())
-							.on('set', this.setTargetState(this));
+							.on('get', this.getTargetState.bind(this))
+							.on('set', this.setTargetState.bind(this));
 
 				return [this.securityService];
 		}
